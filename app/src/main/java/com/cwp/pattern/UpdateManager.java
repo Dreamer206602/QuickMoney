3558295cf4ba6e75c2fc8a3533380284f2006b9c
boobooL <boobooMX@163.com>
2016-04-01 14:54:57 +0800

    修改了部分文件

--- app/src/main/java/com/cwp/pattern/UpdateManager.java
+++ app/src/main/java/com/cwp/pattern/UpdateManager.java
@@ -50,26 +50,26 @@ import android.widget.Toast;
  */
 
 public class UpdateManager extends Service {
-	/* ����د */
+	/* 下载丿 */
 	private static final int DOWNLOAD = 1;
 	private static final int DOWNLOAD2 = 4;
-	/* ���ؽ��� */
+	/* 下载结束 */
 	private static final int DOWNLOAD_FINISH = 2;
 	public static final int IS_FINISH = 0;
 	public static final int DOWNLOAD_FILERR = 3;
-	/* ���������XML��Ϣ */
+	/* 保存解析的XML信息 */
 	HashMap<String, String> mHashMap;
-	/* ���ر���·�� */
+	/* 下载保存路径 */
 	private String mSavePath;
-	/* ��¼���������Y */
+	/* 记录进度条数釿 */
 	private int progress;
-	/* �Ƿ�ȡ������ */
+	/* 是否取消更新 */
 	private boolean cancelUpdate = false;
 
 	private static final int down_step = 3;
 
 	private Context mContext;
-	/* ���½��Ȱ� */
+	/* 更新进度板 */
 	private ProgressBar mProgress;
 	private Dialog mDownloadDialog;
 	public InputStream inputStream;
@@ -86,48 +86,48 @@ public class UpdateManager extends Service {
 	private Handler mHandler = new Handler() {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
-			// ��������
-			case DOWNLOAD:
-				mRoundProgressBar.setProgress(progress);
-				// ���ý�����λ�Z
-				break;
-			case DOWNLOAD_FINISH:
-				// ��װ�ļ�
-				installApk();
-				break;
-			case DOWNLOAD_FILERR:
-				// �������ļ�
-				Toast.makeText(mContext, "�����ļ���������������", Toast.LENGTH_LONG)
-						.show();
-				break;
-			default:
-				break;
+				// 正在下载
+				case DOWNLOAD:
+					mRoundProgressBar.setProgress(progress);
+					// 设置进度条位罿
+					break;
+				case DOWNLOAD_FINISH:
+					// 安装文件
+					installApk();
+					break;
+				case DOWNLOAD_FILERR:
+					// 无下载文件
+					Toast.makeText(mContext, "下载文件出错，请重新下载", Toast.LENGTH_LONG)
+							.show();
+					break;
+				default:
+					break;
 			}
 		};
 	};
 
 	public Handler handler = new Handler() {
-		// ��Handler�л�ȡ��Ϣ����дhandleMessage()����
+		// 在Handler中获取消息，重写handleMessage()方法
 		@Override
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
-			case 1:
-				// �ɹ���������,���޸���
-				if (show.equals("show")) {
-					Toast.makeText(mContext, "���޸���", Toast.LENGTH_LONG).show();
-				}
-				break;
-			case 2:
-				// �и���
-				showNoticeDialog();
-				break;
-			case -1:
-				// ��ס
-				if (show.equals("show")) {
-					Toast.makeText(mContext, "������˵�״��", Toast.LENGTH_LONG)
-							.show();
+				case 1:
+					// 成功建立连接,但无更新
+					if (show.equals("show")) {
+						Toast.makeText(mContext, "暂无更新", Toast.LENGTH_LONG).show();
+					}
 					break;
-				}
+				case 2:
+					// 有更新
+					showNoticeDialog();
+					break;
+				case -1:
+					// 卡住
+					if (show.equals("show")) {
+						Toast.makeText(mContext, "网络出了点状况", Toast.LENGTH_LONG)
+								.show();
+						break;
+					}
 			}
 		}
 	};
@@ -137,7 +137,7 @@ public class UpdateManager extends Service {
 	}
 
 	/**
-	 * ��?�������
+	 * 棿?软件更新
 	 */
 	public void checkUpdate(String show) {
 		this.show = show;
@@ -145,18 +145,18 @@ public class UpdateManager extends Service {
 			isUpdate();
 		} else {
 			if (show.equals("show")) {
-				Toast.makeText(mContext, "��ǰΪ��WIFI���磡", Toast.LENGTH_LONG)
+				Toast.makeText(mContext, "当前为非WIFI网络！", Toast.LENGTH_LONG)
 						.show();
 			}
 		}
 	}
 
 	/**
-	 * ��?����Ƿ��и��°�c
+	 * 棿?软件是否有更新版朿
 	 */
 	public void isUpdate() {
-		// ��ȡ��ǰ����汾
-		// ��version.xml�ŵ������ϣ�Ȼ���ȡ�ļ���Ϣ
+		// 获取当前软件版本
+		// 把version.xml放到网络上，然后获取文件信息
 		// InputStream inStream = ParseXmlService.class.getClassLoader()
 		// .getResourceAsStream("version.xml");
 		new Thread() {
@@ -166,28 +166,28 @@ public class UpdateManager extends Service {
 					double versionCode = getVersionCode(mContext);
 					URL url = new URL(
 							"http://linmp6-wordpress.stor.sinaapp.com/version.xml");
-					// ������������
+					// 建立网络连接
 					HttpURLConnection conn = (HttpURLConnection) url
 							.openConnection();
 					conn.setConnectTimeout(1 * 1000);
-					conn.setReadTimeout(1 * 1000); // ��������
+					conn.setReadTimeout(1 * 1000); // 必须设置
 					inputStream = conn.getInputStream();
-					// ��ȡͼƬ����
+					// 获取图片数据
 					ParseXmlService service = new ParseXmlService();
-					// ����XML�ļ�?����XML�ļ��Ƚ�С�����ʹ��DOM��ʽ���н���
+					// 解析XML文件?由于XML文件比较小，因此使用DOM方式进行解析
 					mHashMap = service.parseXml(inputStream);
 					Message message = new Message();
 					message.what = 1;
 					if (null != mHashMap) {
 						Double serviceCode = Double.valueOf(mHashMap
 								.get("version"));
-						// �汾�ж�
+						// 版本判断
 						if ((serviceCode - versionCode) > 0) {
 							message.what = 2;
 						}
 					}
 					inputStream.close();
-					// ������Ϣ����Ϣ������
+					// 发送消息到消息队列中
 					handler.sendMessage(message);
 				} catch (Exception e) {
 					Message message = new Message();
@@ -200,15 +200,15 @@ public class UpdateManager extends Service {
 	}
 
 	/**
-	 * ��ȡ����汾��
-	 * 
+	 * 获取软件版本叿
+	 *
 	 * @param context
 	 * @return
 	 */
 	public double getVersionCode(Context context) {
 		double versionCode = 0;
 		try {
-			// ��ȡ����汾�ţ���ӦAndroidManifest.xml��android:versionCode
+			// 获取软件版本号，对应AndroidManifest.xml下android:versionCode
 			versionCode = Double.valueOf(
 					context.getPackageManager().getPackageInfo(
 							context.getPackageName(), 0).versionName)
@@ -220,25 +220,25 @@ public class UpdateManager extends Service {
 	}
 
 	/**
-	 * ��ʾ������¶Ի��U
+	 * 显示软件更新对话桿
 	 */
 	private void showNoticeDialog() {
-		// ��??���U
-		Builder builder = new Builder(mContext);
-		builder.setTitle("�������");
-		builder.setMessage("��⵽�°汾" + mHashMap.get("version") + "������������?\n\n"
+		// 构??话桿
+		AlertDialog.Builder builder = new Builder(mContext);
+		builder.setTitle("软件更新");
+		builder.setMessage("检测到新版本" + mHashMap.get("version") + "，立即更新吗?\n\n"
 				+ mHashMap.get("text").replace("\\n", "\n"));
-		// ����
-		builder.setPositiveButton("����", new OnClickListener() {
+		// 更新
+		builder.setPositiveButton("更新", new OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.dismiss();
-				// ��ʾ���ضԻ��U
+				// 显示下载对话桿
 				showDownloadDialog();
 			}
 		});
-		// �Ժ����
-		builder.setNegativeButton("�Ժ����", new OnClickListener() {
+		// 稍后更新
+		builder.setNegativeButton("稍后更新", new OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.dismiss();
@@ -249,13 +249,13 @@ public class UpdateManager extends Service {
 	}
 
 	/**
-	 * ��ʾ������ضԻ��U
+	 * 显示软件下载对话桿
 	 */
 	private void showDownloadDialog() {
-		// ��??�����ضԻ��U
-		Builder builder = new Builder(mContext);
-		builder.setTitle("���ڸ���");
-		// �����ضԻ������ӽ��Ȱ�
+		// 构??件下载对话桿
+		AlertDialog.Builder builder = new Builder(mContext);
+		builder.setTitle("正在更新");
+		// 给下载对话框增加进度板
 		final LayoutInflater inflater = LayoutInflater.from(mContext);
 		View v = inflater.inflate(R.layout.softupdate_progress, null);
 		mRoundProgressBar = (RoundProgressBarWidthNumber) v
@@ -263,39 +263,40 @@ public class UpdateManager extends Service {
 		// mProgress = (ProgressBar) v.findViewById(R.id.update_progress);
 		// update_text = (TextView) v.findViewById(R.id.update_text);
 		builder.setView(v);
-		// ȡ������
-		builder.setNegativeButton("ȡ��", new OnClickListener() {
+		// 取消更新
+		builder.setNegativeButton("取消", new OnClickListener() {
 			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.dismiss();
-				// ����ȡ��״��
+				// 设置取消状濍
 				cancelUpdate = true;
 
 				notification.flags = Notification.FLAG_AUTO_CANCEL;
-				notification.setLatestEventInfo(mContext, mHashMap.get("name"),
-						"�û���ȡ������", null);
+				//TODO   ???
+//				notification.setLatestEventInfo(mContext, mHashMap.get("name"),
+//						"用户已取消下载", null);
 				notificationManager.notify(R.layout.notification_item,
 						notification);
 			}
 		});
 		mDownloadDialog = builder.create();
 		mDownloadDialog.show();
-		// �����ļ�
+		// 现在文件
 		downloadApk();
 	}
 
 	/**
-	 * ����apk�ļ�
+	 * 下载apk文件
 	 */
 	private void downloadApk() {
-		// �������߳��������
+		// 启动新线程下载软仿
 		new downloadApkThread().start();
 	}
 
 	/**
-	 * �����ļ��߳�
-	 * 
+	 * 下载文件线程
+	 *
 	 * @author coolszy
 	 * @date 2012-4-26
 	 * @blog http://blog.92coding.com
@@ -304,49 +305,49 @@ public class UpdateManager extends Service {
 		@Override
 		public void run() {
 			try {
-				// �ж�SD���Ƿ���ڣ������Ƿ���ж�дȨ��
+				// 判断SD卡是否存在，并且是否具有读写权限
 				if (Environment.getExternalStorageState().equals(
 						Environment.MEDIA_MOUNTED)) {
-					// ��ô洢����·��
+					// 获得存储卡的路径
 					String sdpath = Environment.getExternalStorageDirectory()
 							+ "/";
 					mSavePath = sdpath + "download";
 					URL url = new URL(mHashMap.get("url"));
-					// ��������
+					// 创建连接
 					HttpURLConnection conn = (HttpURLConnection) url
 							.openConnection();
 					conn.connect();
-					// ��ȡ�ļ���С
+					// 获取文件大小
 					int length = conn.getContentLength();
-					// ���������
+					// 创建输入浿
 					if (length > -1) {
 						createNotification();
 						InputStream is = conn.getInputStream();
 						File file = new File(mSavePath);
-						// �ж��ļ�Ŀ¼�Ƿ����
+						// 判断文件目录是否存在
 						if (!file.exists()) {
 							file.mkdir();
 						}
 						File apkFile = new File(mSavePath, mHashMap.get("name"));
 						FileOutputStream fos = new FileOutputStream(apkFile);
 						int count = 0;
-						// ����
+						// 缓存
 						byte buf[] = new byte[1024];
-						// д�뵽�ļ���
+						// 写入到文件中
 						do {
 							int numread = is.read(buf);
 							count += numread;
 							progress = (int) (((float) count / length) * 100);
-							// ���½���
+							// 更新进度
 							mHandler.sendEmptyMessage(DOWNLOAD);
 							if (numread <= 0) {
-								// �������
+								// 下载完成
 								mHandler.sendEmptyMessage(DOWNLOAD_FINISH);
 								break;
 							}
-							// д���ļ�
+							// 写入文件
 							fos.write(buf, 0, numread);
-						} while (!cancelUpdate);// ���ȡ����ֹͣ�½�
+						} while (!cancelUpdate);// 点击取消就停止下轿
 						fos.close();
 						is.close();
 					} else {
@@ -359,14 +360,14 @@ public class UpdateManager extends Service {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			// ȡ�����ضԻ����Եj
+			// 取消下载对话框显礿
 			mDownloadDialog.dismiss();
 		}
 	};
 
 	/**
 	 * make true current connect service is wifi
-	 * 
+	 *
 	 * @param mContext
 	 * @return
 	 */
@@ -382,7 +383,7 @@ public class UpdateManager extends Service {
 	}
 
 	/**
-	 * ��װAPK�ļ�
+	 * 安装APK文件
 	 */
 	@SuppressWarnings("deprecation")
 	private void installApk() {
@@ -390,7 +391,7 @@ public class UpdateManager extends Service {
 		if (!apkfile.exists()) {
 			return;
 		}
-		// ͨ��Intent��װAPK�ļ�
+		// 通过Intent安装APK文件
 		Intent i = new Intent(Intent.ACTION_VIEW);
 		i.setDataAndType(Uri.parse("file://" + apkfile.toString()),
 				"application/vnd.android.package-archive");
@@ -399,16 +400,17 @@ public class UpdateManager extends Service {
 		PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, i,
 				0);
 		notification.flags = Notification.FLAG_AUTO_CANCEL;
-		notification.setLatestEventInfo(mContext, mHashMap.get("name"), "���سɹ�",
-				pendingIntent);
+		//TODO    ??????
+//		notification.setLatestEventInfo(mContext, mHashMap.get("name"), "下载成功",
+//				pendingIntent);
 		notificationManager.notify(R.layout.notification_item, notification);
 		/*** stop service *****/
 		stopSelf();
 	}
 
 	/**
-	 * ����������createNotification����
-	 * 
+	 * 方法描述：createNotification方法
+	 *
 	 * @param
 	 * @return
 	 * @see UpdateService
@@ -420,16 +422,16 @@ public class UpdateManager extends Service {
 		// notification = new Notification(R.drawable.dot_enable,app_name +
 		// getString(R.string.is_downing) ,System.currentTimeMillis());
 		notification = new Notification(
-		// R.drawable.video_player,//Ӧ�õ�ͼ��
-				R.drawable.ic_launcher,// Ӧ�õ�ͼ��
-				mHashMap.get("name") + "��������", System.currentTimeMillis());
+				// R.drawable.video_player,//应用的图标
+				R.drawable.ic_launcher,// 应用的图标
+				mHashMap.get("name") + "正在下载", System.currentTimeMillis());
 		notification.flags = Notification.FLAG_ONGOING_EVENT;
 
 		contentView = new RemoteViews(mContext.getPackageName(),
 				R.layout.notification_item);
 		contentView.setTextViewText(R.id.notificationTitle,
-				mHashMap.get("name") + "��������");
-		contentView.setTextViewText(R.id.notificationPercent, "������");
+				mHashMap.get("name") + "正在下载");
+		contentView.setTextViewText(R.id.notificationPercent, "下载中");
 		contentView.setProgressBar(R.id.notificationProgress, 100, 0, false);
 		notification.contentView = contentView;
 
