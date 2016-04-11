d2d880da0882908f2d5a68e197b9d82b13f32d7b
boobooL <boobooMX@163.com>
2016-04-11 17:14:03 +0800

    添加注册登陆的界面

---
+++ app/src/main/java/com/cwp/cmoneycharge/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.cwp.cmoneycharge.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cwp.cmoneycharge.R;
+
+/**
+ * 登陆的界面
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/cwp/cmoneycharge/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.cwp.cmoneycharge.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cwp.cmoneycharge.R;
+//注册的界面
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
