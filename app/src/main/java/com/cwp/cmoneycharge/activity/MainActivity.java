f2a8bf6a45f6158535b9bb0335897200e92bd385
boobooL <boobooMX@163.com>
2016-04-11 17:10:21 +0800

    修改Application的style样式

--- app/src/main/java/com/cwp/cmoneycharge/activity/MainActivity.java
+++ app/src/main/java/com/cwp/cmoneycharge/activity/MainActivity.java
@@ -42,8 +42,6 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         sp = this.getSharedPreferences("preferences", MODE_WORLD_READABLE);
