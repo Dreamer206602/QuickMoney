7938779af026dca9d3cc3c3db0f0236adf6dc516
boobooL <boobooMX@163.com>
2016-04-11 16:57:29 +0800

    修改部分代码

--- app/src/main/java/com/cwp/cmoneycharge/app/SysApplication.java
+++ app/src/main/java/com/cwp/cmoneycharge/app/SysApplication.java
@@ -1,45 +1,47 @@
 package com.cwp.cmoneycharge.app;
+
 import java.util.LinkedList;
 import java.util.List;
+
 import android.app.Activity;
 import android.app.Application;
 
 public class SysApplication extends Application {
-private List<Activity> mList = new LinkedList<Activity>();
-private static SysApplication instance;
+    private List<Activity> mList = new LinkedList<Activity>();
+    private static SysApplication instance;
 
-private SysApplication() {
-}
+    private SysApplication() {
+    }
 
-public synchronized static SysApplication getInstance() {
-   if (null == instance) {
-         instance = new SysApplication();
-      }
-         return instance;
-      }
+    public synchronized static SysApplication getInstance() {
+        if (null == instance) {
+            instance = new SysApplication();
+        }
+        return instance;
+    }
 
-// add Activity
-public void addActivity(Activity activity) {
-      mList.add(activity);
-}
+    // add Activity
+    public void addActivity(Activity activity) {
+        mList.add(activity);
+    }
 
     public void exit() {
         try {
-           for (Activity activity : mList) {
-              if (activity != null)
-                 activity.finish();
-                 }
-             } catch (Exception e) {
-                 e.printStackTrace();
-             } finally {
-                System.exit(0);
-             }
-}
+            for (Activity activity : mList) {
+                if (activity != null)
+                    activity.finish();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+    }
 
-@Override
-public void onLowMemory() {
-    super.onLowMemory();
-      System.gc();
-}
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        System.gc();
+    }
 
 }
