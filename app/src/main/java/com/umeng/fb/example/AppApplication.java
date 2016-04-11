bb55b67bacf196e1dfe10629c86714fb47e3f7a8
boobooL <boobooMX@163.com>
2016-04-11 17:30:29 +0800

    修改部分代码

--- app/src/main/java/com/umeng/fb/example/AppApplication.java
+++ app/src/main/java/com/umeng/fb/example/AppApplication.java
@@ -3,9 +3,7 @@ package com.umeng.fb.example;
 import android.app.Application;
 import com.umeng.fb.push.FeedbackPush;
 
-/**
- * Created by user on 2014/10/13.
- */
+
 public class AppApplication extends Application {
     @Override
     public void onCreate() {
