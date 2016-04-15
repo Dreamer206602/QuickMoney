e0fadd3cca68086b41a779408ef9e00010dc3f49
boobooL <boobooMX@163.com>
2016-04-15 13:55:18 +0800

    修改部分代码

--- app/src/main/java/com/cwp/cmoneycharge/activity/LoginActivity.java
+++ app/src/main/java/com/cwp/cmoneycharge/activity/LoginActivity.java
@@ -2,17 +2,32 @@ package com.cwp.cmoneycharge.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cwp.cmoneycharge.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * 登陆的界面
  */
 public class LoginActivity extends Activity {
 
+    @Bind(R.id.username)
+    EditText mUsername;
+    @Bind(R.id.password)
+    EditText mPassword;
+    @Bind(R.id.btnCancle)
+    Button mBtnCancle;
+    @Bind(R.id.btnLogin)
+    Button mBtnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/cwp/cmoneycharge/activity/RegisterActivity.java
+++ app/src/main/java/com/cwp/cmoneycharge/activity/RegisterActivity.java
@@ -2,14 +2,35 @@ package com.cwp.cmoneycharge.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableRow;
 
 import com.cwp.cmoneycharge.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 //注册的界面
 public class RegisterActivity extends Activity {
+
+    @Bind(R.id.rusername)
+    EditText mRusername;
+    @Bind(R.id.rpassword)
+    EditText mRpassword;
+    @Bind(R.id.rrpassword)
+    EditText mRrpassword;
+    @Bind(R.id.tablerow4)
+    TableRow mTablerow4;
+    @Bind(R.id.btnrCancle)
+    Button mBtnrCancle;
+    @Bind(R.id.btnrRegister)
+    Button mBtnrRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.register);
+        ButterKnife.bind(this);
     }
 }
